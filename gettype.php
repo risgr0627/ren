f101db7dce7b3d39fb6820e92f64f7d14f16ca82
risgr0627 <risgr2000@gmail.com>
2022-02-07 19:22:41 +0900

    first commit

---
+++ gettype.php
@@ -0,0 +1,10 @@
+<?php
+
+$str = '文字列です';
+$int_number = 4;
+$arr = array(1,2,3,4);
+
+echo gettype($str).PHP_EOL;
+echo gettype($int_number).PHP_EOL;
+echo gettype($arr).PHP_EOL;
+?>
---
+++ tax_function.php
@@ -0,0 +1,15 @@
+<?php
+
+class tax{
+    public $price = 1000;
+    public $taxRate = 0.1;
+    public $calculation = ;
+    function calculationResult(){
+        echo $this->calculation.PHP_EOL;
+    }
+    
+}
+
+
+
+?>
