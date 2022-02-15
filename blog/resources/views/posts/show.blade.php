2dcac2459ec0d8dc35b76803d805ccd46f210b89
risgr0627 <risgr2000@gmail.com>
2022-02-15 21:11:16 +0900

    9-2

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -22,6 +22,7 @@
                 <p class='body'>{{ $post->body }}</p>
                 <p class='updated_at'>{{ $post->updated_at }}</p>
             </div>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
             <div class='back'>[<a href='/'>back</a>]</div>
             <script>
                 function deletePost(e){
